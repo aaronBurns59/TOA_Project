1767185924c22a4e96607b365c86993de1e66f26
aaronburns59 <aaronburns59@gmail.com>
2020-01-16 13:18:40 +0000

    Set up the basics for the project by creating the c file

---
+++ MD5.c
@@ -0,0 +1,9 @@
+// Aaron Burns
+// An Algorithm that calculates the MD5 hash digest of any input
+
+#include <stdio.h>
+
+main()
+{
+
+}// main
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TOA_Project
+## MD5 Hash Algorithm
+
+## Repository Layout
+
+## Set Up
+
+## How to Run
+
+## Research
+
+## References
