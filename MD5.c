9839e0a69c15a0463e788a9b8505f357ab3161e5
aaronburns59 <aaronburns59@gmail.com>
2020-03-12 02:09:31 +0000

    Refactored hash function slightly

--- MD5.c
+++ MD5.c
@@ -11,10 +11,10 @@
 
 // Four "Words"(32-bit) initialized  in low order hexidecimal
 // These are usd in each round of the MD5 operation
-#define A 0x67452301
-#define B 0xefcdab89
-#define C 0x98badcfe
-#define D 0x10325476
+#define WordA 0x67452301
+#define WordB 0xefcdab89
+#define WordC 0x98badcfe
+#define WordD 0x10325476
 
 // MD5 Rotation Constants
 // The values in K will be used in each step of the MD5 Algorithm
@@ -101,7 +101,6 @@ static uint32_t F(uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t m, in
         shiftAmount = s[0][i];
         // printf("Accessing s elemetents: %d\n", shiftAmount);
     }// for
-    printf("%d\n", shiftAmount);
     a += AuxF(b,c,d) + m + k;
     a = ROTL(a, shiftAmount);
     return a += b;
@@ -113,7 +112,6 @@ static uint32_t G(uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t m, in
     {
         shiftAmount = s[1][si];
     }// for
-    printf("%d\n", shiftAmount);
     a += AuxG(b,c,d) + m + k;
     a = ROTL(a, shiftAmount);
     a += b;
@@ -125,7 +123,6 @@ static uint32_t H(uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t m, in
     {
         shiftAmount = s[2][si];
     }// for
-    printf("%d\n", shiftAmount);
     a += AuxH(b,c,d) + m + k;
     a = ROTL(a, shiftAmount);
     a += b;
@@ -137,7 +134,6 @@ static uint32_t I(uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t m, in
     {
         shiftAmount = s[3][si];
     }// for
-    printf("%d\n", shiftAmount);
     a += AuxI(b,c,d) + m + k;
     a = ROTL(a, shiftAmount);
     a += b;
@@ -211,74 +207,82 @@ uint32_t hashMD5(BLOCK *M){
     // M and K is the select the shift amounts from the s[i][j] 2d array constant, see the method F,G,H or I to see how 
     // that works
 
+    int A=WordA,B=WordB,C=WordC,D=WordD;
+    int a = A, b = B, c = C, d = D;
+
     // =====Round 1===================Operation
-    F(A,B,C,D,M->threeTwo[0],0,K[0]);   //  1
-    F(D,A,B,C,M->threeTwo[1],1,K[1]);   //  2
-    F(C,D,A,B,M->threeTwo[2],2,K[2]);   //  3
-    F(B,C,D,A,M->threeTwo[3],3,K[3]);   //  4
-    F(A,B,C,D,M->threeTwo[4],0,K[4]);   //  5
-    F(D,A,B,C,M->threeTwo[5],1,K[5]);   //  6
-    F(C,D,A,B,M->threeTwo[6],2,K[6]);   //  7
-    F(B,C,D,A,M->threeTwo[7],3,K[7]);   //  8
-    F(A,B,C,D,M->threeTwo[8],0,K[8]);   //  9
-    F(D,A,B,C,M->threeTwo[9],1,K[9]);   // 10
-    F(C,D,A,B,M->threeTwo[10],2,K[10]); // 11
-    F(B,C,D,A,M->threeTwo[11],3,K[11]); // 12
-    F(A,B,C,D,M->threeTwo[12],0,K[12]); // 13
-    F(D,A,B,C,M->threeTwo[13],1,K[13]); // 14
-    F(C,D,A,B,M->threeTwo[14],2,K[14]); // 15
-    F(B,C,D,A,M->threeTwo[15],3,K[15]); // 16
+    F(a,b,c,d,M->threeTwo[0],0,K[0]);   //  1
+    F(d,a,b,c,M->threeTwo[1],1,K[1]);   //  2
+    F(c,d,a,b,M->threeTwo[2],2,K[2]);   //  3
+    F(b,c,d,a,M->threeTwo[3],3,K[3]);   //  4
+    F(a,b,c,d,M->threeTwo[4],0,K[4]);   //  5
+    F(d,a,b,c,M->threeTwo[5],1,K[5]);   //  6
+    F(c,d,a,b,M->threeTwo[6],2,K[6]);   //  7
+    F(b,c,d,a,M->threeTwo[7],3,K[7]);   //  8
+    F(a,b,c,d,M->threeTwo[8],0,K[8]);   //  9
+    F(d,a,b,c,M->threeTwo[9],1,K[9]);   // 10
+    F(c,d,a,b,M->threeTwo[10],2,K[10]); // 11
+    F(b,c,d,a,M->threeTwo[11],3,K[11]); // 12
+    F(a,b,c,d,M->threeTwo[12],0,K[12]); // 13
+    F(d,a,b,c,M->threeTwo[13],1,K[13]); // 14
+    F(c,d,a,b,M->threeTwo[14],2,K[14]); // 15
+    F(b,c,d,a,M->threeTwo[15],3,K[15]); // 16
     // =====Round 2===================Operation
-    G(A,B,C,D,M->threeTwo[1],0,K[16]);  // 17
-    G(D,A,B,C,M->threeTwo[6],1,K[17]);  // 18
-    G(C,D,A,B,M->threeTwo[11],2,K[18]); // 19
-    G(B,C,D,A,M->threeTwo[0],3,K[19]);  // 20
-    G(A,B,C,D,M->threeTwo[5],0,K[20]);  // 21
-    G(D,A,B,C,M->threeTwo[10],1,K[21]); // 22
-    G(C,D,A,B,M->threeTwo[15],2,K[22]); // 23
-    G(B,C,D,A,M->threeTwo[4],3,K[23]);  // 24
-    G(A,B,C,D,M->threeTwo[9],0,K[24]);  // 25
-    G(D,A,B,C,M->threeTwo[14],1,K[25]); // 26
-    G(C,D,A,B,M->threeTwo[3],2,K[26]);  // 27
-    G(B,C,D,A,M->threeTwo[8],3,K[27]);  // 28
-    G(A,B,C,D,M->threeTwo[13],0,K[28]); // 29
-    G(D,A,B,C,M->threeTwo[2],1,K[29]);  // 30
-    G(C,D,A,B,M->threeTwo[7],2,K[30]);  // 31
-    G(B,C,D,A,M->threeTwo[12],3,K[31]); // 32
+    G(a,b,c,d,M->threeTwo[1],0,K[16]);  // 17
+    G(d,a,b,c,M->threeTwo[6],1,K[17]);  // 18
+    G(c,d,a,b,M->threeTwo[11],2,K[18]); // 19
+    G(b,c,d,a,M->threeTwo[0],3,K[19]);  // 20
+    G(a,b,c,d,M->threeTwo[5],0,K[20]);  // 21
+    G(d,a,b,c,M->threeTwo[10],1,K[21]); // 22
+    G(c,d,a,b,M->threeTwo[15],2,K[22]); // 23
+    G(b,c,d,a,M->threeTwo[4],3,K[23]);  // 24
+    G(a,b,c,d,M->threeTwo[9],0,K[24]);  // 25
+    G(d,a,b,c,M->threeTwo[14],1,K[25]); // 26
+    G(c,d,a,b,M->threeTwo[3],2,K[26]);  // 27
+    G(b,c,d,a,M->threeTwo[8],3,K[27]);  // 28
+    G(a,b,c,d,M->threeTwo[13],0,K[28]); // 29
+    G(d,a,b,c,M->threeTwo[2],1,K[29]);  // 30
+    G(c,d,a,b,M->threeTwo[7],2,K[30]);  // 31
+    G(b,c,d,a,M->threeTwo[12],3,K[31]); // 32
     // =====Round 3===================Operation
-    H(A,B,C,D,M->threeTwo[5],0,K[32]);  // 33
-    H(D,A,B,C,M->threeTwo[8],1,K[33]);  // 34
-    H(C,D,A,B,M->threeTwo[11],2,K[34]); // 35
-    H(C,D,A,B,M->threeTwo[14],3,K[35]); // 36
-    H(A,B,C,D,M->threeTwo[1],0,K[36]);  // 37
-    H(D,A,B,C,M->threeTwo[4],1,K[37]);  // 38
-    H(C,D,A,B,M->threeTwo[7],2,K[38]);  // 39
-    H(C,D,A,B,M->threeTwo[10],3,K[39]); // 40
-    H(A,B,C,D,M->threeTwo[13],0,K[40]); // 41
-    H(D,A,B,C,M->threeTwo[0],1,K[41]);  // 42
-    H(C,D,A,B,M->threeTwo[3],2,K[42]);  // 43
-    H(C,D,A,B,M->threeTwo[6],3,K[43]);  // 44
-    H(A,B,C,D,M->threeTwo[9],0,K[44]);  // 45
-    H(D,A,B,C,M->threeTwo[12],1,K[45]); // 46
-    H(C,D,A,B,M->threeTwo[15],2,K[46]); // 47
-    H(C,D,A,B,M->threeTwo[2],3,K[47]);  // 48
+    H(a,b,c,d,M->threeTwo[5],0,K[32]);  // 33
+    H(d,a,b,c,M->threeTwo[8],1,K[33]);  // 34
+    H(c,d,a,b,M->threeTwo[11],2,K[34]); // 35
+    H(b,c,d,a,M->threeTwo[14],3,K[35]); // 36
+    H(a,b,c,d,M->threeTwo[1],0,K[36]);  // 37
+    H(d,a,b,c,M->threeTwo[4],1,K[37]);  // 38
+    H(c,d,a,b,M->threeTwo[7],2,K[38]);  // 39
+    H(b,c,d,a,M->threeTwo[10],3,K[39]); // 40
+    H(a,b,c,d,M->threeTwo[13],0,K[40]); // 41
+    H(d,a,b,c,M->threeTwo[0],1,K[41]);  // 42
+    H(c,d,a,b,M->threeTwo[3],2,K[42]);  // 43
+    H(b,c,d,a,M->threeTwo[6],3,K[43]);  // 44
+    H(a,b,c,d,M->threeTwo[9],0,K[44]);  // 45
+    H(d,a,b,c,M->threeTwo[12],1,K[45]); // 46
+    H(c,d,a,b,M->threeTwo[15],2,K[46]); // 47
+    H(b,c,d,a,M->threeTwo[2],3,K[47]);  // 48
     // =====Round 4===================Operation
-    I(A,B,C,D,M->threeTwo[0],0,K[48]);  // 49
-    I(D,A,B,C,M->threeTwo[7],1,K[49]);  // 50
-    I(C,D,A,B,M->threeTwo[14],2,K[50]); // 51
-    I(B,C,D,A,M->threeTwo[5],3,K[51]);  // 52
-    I(A,B,C,D,M->threeTwo[12],0,K[52]); // 53
-    I(D,A,B,C,M->threeTwo[3],1,K[53]);  // 54
-    I(C,D,A,B,M->threeTwo[10],2,K[54]); // 55
-    I(B,C,D,A,M->threeTwo[1],3,K[55]);  // 56
-    I(A,B,C,D,M->threeTwo[8],0,K[56]);  // 57
-    I(D,A,B,C,M->threeTwo[15],1,K[57]);  // 58
-    I(C,D,A,B,M->threeTwo[6],2,K[58]); // 59
-    I(B,C,D,A,M->threeTwo[13],3,K[59]);  // 60
-    I(A,B,C,D,M->threeTwo[4],0,K[60]); // 61
-    I(D,A,B,C,M->threeTwo[11],1,K[61]);  // 62
-    I(C,D,A,B,M->threeTwo[2],2,K[62]); // 63
-    I(B,C,D,A,M->threeTwo[9],3,K[63]);  // 64
+    I(a,b,c,d,M->threeTwo[0],0,K[48]);  // 49
+    I(d,a,b,c,M->threeTwo[7],1,K[49]);  // 50
+    I(c,d,a,b,M->threeTwo[14],2,K[50]); // 51
+    I(b,c,d,a,M->threeTwo[5],3,K[51]);  // 52
+    I(a,b,c,d,M->threeTwo[12],0,K[52]); // 53
+    I(d,a,b,c,M->threeTwo[3],1,K[53]);  // 54
+    I(c,d,a,b,M->threeTwo[10],2,K[54]); // 55
+    I(b,c,d,a,M->threeTwo[1],3,K[55]);  // 56
+    I(a,b,c,d,M->threeTwo[8],0,K[56]);  // 57
+    I(d,a,b,c,M->threeTwo[15],1,K[57]); // 58
+    I(c,d,a,b,M->threeTwo[6],2,K[58]);  // 59
+    I(b,c,d,a,M->threeTwo[13],3,K[59]); // 60
+    I(a,b,c,d,M->threeTwo[4],0,K[60]);  // 61
+    I(d,a,b,c,M->threeTwo[11],1,K[61]); // 62
+    I(c,d,a,b,M->threeTwo[2],2,K[62]);  // 63
+    I(b,c,d,a,M->threeTwo[9],3,K[63]);  // 64
+
+    A += a;
+    B += b;
+    C += c;
+    D += d;
 
 }   
 
@@ -309,6 +313,8 @@ int main(int argc, char *argv[]){
     {
         hashMD5(&M);
     }// while
+
+    printf("%02" PRIX32, hashMD5(&M));
 
     // Closing file
     fclose(infile);
